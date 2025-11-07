22bd29094502d1db44e946ffe45f98e14746d55b
Benjamin Andres Paba Gonzalez <bpaba@mutualser.org>
2025-11-07 12:29:28 -0500

    devtalles-> some changes

--- src/ScannerExample/NumberCompare.java
+++ src/ScannerExample/NumberCompare.java
@@ -6,7 +6,7 @@ public class NumberCompare {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number1 = SimpleCalculator.getNumber(scanner, "Ingresa un numero: ");
-        int number2 = SimpleCalculator.getNumber(scanner, "Ingresa otro numero: ");;
+        int number2 = SimpleCalculator.getNumber(scanner, "Ingresa otro numero: ");
 
         SimpleCalculator.printArithmeticOperation(number1, number2);
 
